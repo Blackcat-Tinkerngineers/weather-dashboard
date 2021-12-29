d6217ed3d77bc1d95217b60c496211b21b54e2f0
Chelsie L Bangloy-Gonsalves <clbgonsalves@gmail.com>
2021-12-28 22:13:18 -1000

    git update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 var getUserRepos = function() {
-  fetch("https://api.openweathermap.org/data/2.5/weather?zip=89502,us&appid=a33b47c3058109d2a290e7b9d0c345ca");
+  fetch("https://api.weather.gov/openapi.json");
 };
 
-var response = fetch("https://api.openweathermap.org/data/2.5/weather?zip=89502,us&appid=a33b47c3058109d2a290e7b9d0c345ca");
+var response = fetch("https://api.weather.gov/openapi.json");
 console.log(response);
 
-fetch("https://api.openweathermap.org/data/2.5/weather?zip=89502,us&appid=a33b47c3058109d2a290e7b9d0c345ca").then(function(response) {
+fetch("https://api.weather.gov/openapi.json").then(function(response) {
   response.json().then(function(data) {
     console.log(data);
   });
